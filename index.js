ff99073c29c1d0ba4b289bd8a4091c4508e83d3b
Hoang Tran <jashoang2583@gmail.com>
2022-10-12 15:12:33 -0700

    commit tree 324cd3a5c67300dd682c021f3e6ea7483cecfc6a

---
+++ index.js
@@ -0,0 +1,23 @@
+/* function divide(a, b){
+    a = 2000;
+    b = 100;
+    const divide = */
+
+const divide = (a, b) => a / b;
+
+
+const square = (x) => x**2;
+
+const add = (a, b) => a + b;
+    
+
+
+/* function square(arrowFunction) {
+    return arrowFunction() * arrowFunction();
+}*/
+
+
+
+
+
+   
